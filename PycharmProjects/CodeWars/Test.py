5f70c59397c35d8c75e22b80a177ffdc0072dac3
Vsevalot <v.v.tetervak@gmail.com>
2018-01-30 13:45:33 +0500

    asdf

--- PycharmProjects/CodeWars/Test.py
+++ PycharmProjects/CodeWars/Test.py
@@ -1,18 +1,5 @@
-import matplotlib.pyplot as plt
+import mypyfunctions as myPy
 
+path = "Z:\\Tetervak\\skipped_records_20180129_110000_sorted.xlsx"
+table = myPy.readXLSX(path)
 
-plt.subplot(211)
-plt.plot([1,2,3], label="test1")
-plt.plot([3,2,1], label="test2")
-# Place a legend above this subplot, expanding itself to
-# fully use the given bounding box.
-plt.legend(bbox_to_anchor=(0., 1.02, 1., .102), loc=3,
-           ncol=2, mode="expand", borderaxespad=0.)
-
-plt.subplot(223)
-plt.plot([1,2,3], label="test1")
-plt.plot([3,2,1], label="test2")
-# Place a legend to the right of this smaller subplot.
-plt.legend(bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
-
-plt.show()
