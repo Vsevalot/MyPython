462a5e2e7bbf27f8692b6323a45d0f3ca711f660
Vsevalot <v.v.tetervak@gmail.com>
2018-02-13 14:26:49 +0500

    read red

--- PycharmProjects/CodeWars/Test.py
+++ PycharmProjects/CodeWars/Test.py
@@ -1,5 +1,19 @@
-import mypyfunctions as myPy
+import openpyxl
 
-path = "Z:\\Tetervak\\skipped_records_20180129_110000_sorted.xlsx"
-table = myPy.readXLSX(path)
+path = "Z:\\Tetervak\\skipped_records_20180212_162000.xlsx"
 
+def findReds(xlsx_path: str) -> list:
+    from openpyxl import load_workbook
+    wb = load_workbook(filename=xlsx_path, read_only=True)
+    sheet = wb[wb.get_sheet_names()[0]]
+    reds = []
+    bad = wb.style_names.index("Плохой")
+    for row in sheet.rows:
+        for cell in row:
+            if cell.value is not None:
+                if cell._style_id == bad and cell.value[1:4] == 'rec':
+                    reds.append(str(cell.value))
+    return reds
+
+
+print(findReds(path))
